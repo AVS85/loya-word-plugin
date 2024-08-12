2c03c0900e30c51e8af670330c3a71300e744c71
ShkalikovAV <shkalikov@atb.su>
2024-08-13 00:28:04 +0300

    is support api

--- src/taskpane/store/config.ts
+++ src/taskpane/store/config.ts
@@ -16,9 +16,9 @@ class ConfigStore {
     this.rootStore = rootStore;
 
     autorun(() => {
-      Office.onReady((info) => {
+      Office.onReady(async (info) => {
         /** Включение записи исправлений при запуске плагина */
-        Word.run(async (context) => {
+        await Word.run(async (context) => {
           context.document.changeTrackingMode = Word.ChangeTrackingMode.trackAll;
           await context
             .sync()
@@ -27,15 +27,19 @@ class ConfigStore {
         });
 
         /** Определение поддерживаемых api word'a функций плагина */
+        const { platform } = info;
+        const isPlatformOnline = platform === Office.PlatformType.OfficeOnline;
+
         const isApiExist_1_1 = Office.context.requirements.isSetSupported("WordApi", "1.1");
         const isApiExist_1_3 = Office.context.requirements.isSetSupported("WordApi", "1.3");
         const isApiExist_1_4 = Office.context.requirements.isSetSupported("WordApi", "1.4");
         const isApiExist_1_6 = Office.context.requirements.isSetSupported("WordApi", "1.6");
+
         const optionsSupportedCurrentApi = {
           searchInBody: isApiExist_1_3,
           rangeInsertComment: isApiExist_1_4,
           rangeInsertText: isApiExist_1_1,
-          rangeInsertTextSemantic: isApiExist_1_6,
+          rangeInsertTextSemantic: isPlatformOnline ? false : isApiExist_1_6, //TODO: нужен фикс для онлайн версии
         };
         runInAction(() => {
           this.optionsSupportedCurrentApi = optionsSupportedCurrentApi;
